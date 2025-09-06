2f8fa0d7a49839f6df8e2a6463117dc849c70508
williamloosman <william.loosman@kadaster.nl>
2025-09-06 18:23:50 +0200

    Add Mbale

--- src/assets/datasetConfig.js
+++ src/assets/datasetConfig.js
@@ -2,6 +2,9 @@ export const DATASET_CONFIG = {
   Mombasa: {
     url: "https://services.digireg.com/wms/test/mombasa/wms?",
   },
+  Mbale: {
+    url: "https://services.digireg.com/mapserver/mbale?",
+  },
 //   BAG: {
 //     url: "https://service.pdok.nl/lv/bag/wms/v2_0?",
 //   },
